ffdb22e04ecba9564944cf298f5712afef632f99
Viktoriia Tomchuk <79446102+rxndx@users.noreply.github.com>
2022-11-18 23:42:11 +0200

    Update ProgramCodeModified.java

--- ProgramCodeModified.java
+++ ProgramCodeModified.java
@@ -61,6 +61,26 @@ public final class setHealthConditionTest {
                 && healthConditionPressure > MIN_CONDITION_ID && healthConditionPressure < MAX_CONDITION_ID
                 && healthConditionPulse > MIN_CONDITION_ID && healthConditionPulse < MAX_CONDITION_ID;
     }
+    
+        public static boolean isDateCorrect(healthCondition healthCondition) {
+        return healthCondition.getDate().matches("^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]))\\1|(?:(?:29|30)(\\/|-|\\.)(?:0?[13-9]|1[0-2])\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:29(\\/|-|\\.)0?2\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\.)(?:(?:0?[1-9])|(?:1[0-2]))\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$");
+    }
+    
+        public static int setHealthCondition(healthCondition healthCondition) {
+        if (!isIdCorrect(healthCondition)) {
+            return -1;
+        }
+
+        if(!isDateCorrect(healthCondition)) {
+            return -2;
+        }
+
+        if (!areValuesCorrect(healthCondition)) {
+            return -3;
+        }
+
+        return 1;
+    }
 
    /**
    * Main class.
